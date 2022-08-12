173f442a5b36089e4d3b185e317f31ce31fb6c58
itsnicolearias <nicolmccann@gmail.com>
2022-08-12 11:45:25 -0300

    add error handler

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 const express = require('express')
+const createError = require('http-errors')
 const { envConfig } = require('./config/envConfig')
 const { sequelize } =require('./config/database')
 
-require('./models/user')
-require('./models/role')
-require('./models/post')
-require('./models/comment')
-require('./models/message')
+//require('./models/user')
+//require('./models/role')
+//require('./models/post')
+//require('./models/comment')
+//require('./models/message')
 
 const app = express()
 
 app.use(express.json)
 
 sequelize.sync({ alter: true })
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404))
+  })
+  
+  // error handler
+  app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+  
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+  })
 
 const PORT = envConfig.app.port
 
